231b4f1926d356e108e57044f09a59f26107ff6d
Maureen <maureen.gavilan@sansano.usm.cl>
2023-01-31 20:51:26 -0300

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,27 @@ const express = require('express');
 const routes = express.Router()
 
 routes.get('/', (req,res) => {
-    res.send('api')
+    req.getConnection((err,conn) => {
+        if(err) return res.send(err);
+
+        conn.query('SELECT * FROM reportes', (err,rows) => {
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+routes.post('/', (req,res) => {
+    req.getConnection((err,conn) => {
+        if(err) return res.send(err);
+
+        conn.query('INSERT INTO reportes set ?',[req.body] ,(err,rows) => {
+            if(err) return res.send(err)
+
+            res.send('dato insertado');
+        })
+    })
 })
 
 module.exports = routes;
