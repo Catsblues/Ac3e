4a41bc5409880e94f613b1b08851ea5b60dbb834
Maureen <maureen.gavilan@sansano.usm.cl>
2023-02-16 09:52:36 -0300

    Update apiRespaldos.py

--- respaldosa8/apiRespaldos.py
+++ respaldosa8/apiRespaldos.py
@@ -2,28 +2,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/send_file', methods=['POST'])
-def send_file():
+@app.route('/', methods=['GET'])
+def index():
+    response = {"message": "Hello World"}
+    return response,200
 
-    file = request.files['file']
-    file_name = file.filename
 
-    file.save(file_name)
-
-    return 'File saved', 200
-
-@app.route('/get_file', methods=['GET'])
-def get_file():
-
-    file_name = request.json["file_name"]
-    print("file_name: ")
-    print(file_name)
-    file = open(file_name, 'rb')
-
-    content = file.read()
-    file.close()
-
-    return content, 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
