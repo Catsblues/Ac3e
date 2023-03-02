db5888ffba556acbf4e8a2e8dbb1693244b553e5
Maureen <maureen.gavilan@sansano.usm.cl>
2023-03-02 16:32:57 -0300

    commit

--- ac3e/src/Admin/InicioAdmin.js
+++ ac3e/src/Admin/InicioAdmin.js
@@ -1,5 +1,6 @@
 import "./InicioAdmin.css";
-import Modal from "./ComponentsAdmin/Modal.js"
+import Modal from "./ComponentsAdmin/Modal.js";
+import ChangePass from "../Inicio/Components_Inicio/ChangePass";
 import Exit from "../Login/Exit.js";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -7,6 +8,8 @@ import jwt_decode from "jwt-decode";
 
 const InicioAdmin=()=> {
 
+  const [name_researcher, setName_researcher] = useState("");
+  const [show, setShow] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,6 +22,9 @@ const InicioAdmin=()=> {
       if (decodedToken.rol !== "admin") {
         navigate("/inicio");
       }
+      else{
+        setName_researcher(decodedToken.name);
+      }
     }
   }, []);
   return (
@@ -27,9 +33,10 @@ const InicioAdmin=()=> {
     <div className='header'> 
       <img className="logo" src={"/ac3e.png"}/>
       <div className='usuario'>
-        <h1 className="titulo1">Welcome administrator </h1>
+        <h1 className="titulo1">Welcome {name_researcher} </h1>
       </div>
-      <a className="statistics" href="https://app.powerbi.com/view?r=eyJrIjoiOGFhN2I3MzQtY2FlZS00YjQzLWIzNTktNTgwNDNmMWU1MTQxIiwidCI6IjAyNjI1Njc2LTMyMjctNDQwYS05YzY4LWJiNmQyOWRlNDIwNiIsImMiOjR9" target="_blank">Statistics</a> 
+      <a className="statistics" href="https://app.powerbi.com/view?r=eyJrIjoiOGFhN2I3MzQtY2FlZS00YjQzLWIzNTktNTgwNDNmMWU1MTQxIiwidCI6IjAyNjI1Njc2LTMyMjctNDQwYS05YzY4LWJiNmQyOWRlNDIwNiIsImMiOjR9" target="_blank">Statistics</a>
+      <button className="update" onClick={()=>setShow(true)}>Update password</button>
       <button className="exit" onClick={Exit}>Exit</button> 
     </div>
     <h1 className="title">Indicator Report</h1>
@@ -37,7 +44,7 @@ const InicioAdmin=()=> {
     <div className="modalcampos">
       {<Modal/>} 
       </div>
-
+      < ChangePass show={show} onClose={()=>setShow(false)} name={name_researcher}/>
     </>
   );
 }
