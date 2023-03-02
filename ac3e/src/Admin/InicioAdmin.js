132f8ef8e5d6534f8faeeaaad60aa6f67324d574
Maureen <maureen.gavilan@sansano.usm.cl>
2023-03-02 16:47:12 -0300

    t

--- ac3e/src/Admin/InicioAdmin.js
+++ ac3e/src/Admin/InicioAdmin.js
@@ -2,7 +2,7 @@ import "./InicioAdmin.css";
 import Modal from "./ComponentsAdmin/Modal.js";
 import ChangePass from "../Inicio/Components_Inicio/ChangePass";
 import Exit from "../Login/Exit.js";
-import { useEffect } from "react";
+import { useEffect ,useState} from "react";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
